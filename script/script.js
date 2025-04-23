0473a22a3ac89df7a77490f65114b23c192d208f
Vitor Cyriaco <vitorcyriaco@outlook.com>
2025-04-23 14:31:39 -0300

    feat: created departments menu

--- script/script.js
+++ script/script.js
@@ -4,3 +4,10 @@ fetch('../components/allCategoriesMenu.html')
 .then(data => {
   document.getElementById('all-categories').insertAdjacentHTML('beforeend', data);
 });
+
+// Fetch the HTML content of the departmentsMenu.html file and insert it into the element with ID 'departments-menu'
+fetch('../components/departmentsMenu.html')
+.then(res => res.text())
+.then(data => {
+  document.getElementById('departments-menu').insertAdjacentHTML('beforeend', data);
+});
